e44d114071a415b7ffb8c74fd0b850c744c9c27f
cjrzh <cjrzh@163.com>
2021-12-25 22:45:21 +0800

    learned ch8

---
+++ ch8/pizza.py
@@ -0,0 +1,7 @@
+def make_pizza(*toppings):
+    print("\nMaking a pizza with the following toppings:")
+    for top in toppings:
+        print(f"- {top}")
+    
+make_pizza('pep','ma')
+make_pizza('dick','pussy')
